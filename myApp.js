e0ddce9e9c7b3d28961bed0a834bc192ef331e84
matt arias <mattwork07@gmail.com>
2022-02-13 20:41:14 +0000

    add logger from middleware

--- myApp.js
+++ myApp.js
@@ -9,18 +9,23 @@ console.log("hello world")
 
 app.get('/' ,function(req, res){
   res.sendFile(__dirname + '/views/'+'index.html')
+});
+
+app.use(function(req,res,next){
+  console.log('incoming req '+req.method +' '+ req.path+' '+'-'+' '+req.ip);
+  next();
 })
 
-app.use('/public',express.static(__dirname+'/public'))
+// app.use('/public',express.static(__dirname+'/public'));
 
 
-app.get('/json', function(req, res){
-  if(process.env['MESSAGE_STYLE'] === "uppercase"){
-    res.json({"message": "HELLO JSON"})
-  }else{
-    res.json({"message": "Hello json"})
-  }
-})
+// app.get('/json', function(req, res){
+//   if(process.env['MESSAGE_STYLE'] === "uppercase"){
+//     res.json({"message": "HELLO JSON"})
+//   }else{
+//     res.json({"message": "Hello json"})
+//   }
+// });
 
 
 
